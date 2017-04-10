6dde38be475de12dae40d07375114ad0d33ac6df
Fraser Cadger <frasercadger@gmail.com>
2017-04-10 20:50:17 +0100

    Add skeleton main.c
    TODO: Add simple makefile

---
+++ pbm_copy/src/main.c
@@ -0,0 +1,35 @@
+/*
+    Copyright: Fraser Cadger (2017) <frasercadger@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Description: copies a source .pbm file into a new .pbm file using 
+    libnetpbm functions. The purpose is to show how these functions are
+    used to open the original, read in the data, generate an empty pbm
+    + header, then fill it with the image data.
+*/
+
+/* TODO: Includes */
+
+/* TODO: Constants */
+
+/* TODO: Function prototypes */
+
+/* TODO: main() definition */
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
+/* TODO: Function definitions */
